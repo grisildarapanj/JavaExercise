58f681806196f190efd43b4a9b56e7f08bf5d33c
Grisilda Rapanj <grisilda.rapanj@mozenda.net>
2022-03-14 14:15:33 +0100

    changes to exercise two

--- src/main/java/com/hackerrank/sample/controller/SampleController.java
+++ src/main/java/com/hackerrank/sample/controller/SampleController.java
@@ -17,6 +17,17 @@ import org.springframework.web.client.RestTemplate;
 import com.hackerrank.sample.dto.FilteredProducts;
 import com.hackerrank.sample.dto.SortedProducts;
 
+// Exercise 2 part
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+import netscape.javascript.JSObject;
+import java.net.*;
+
 @RestController
 public class SampleController {
 
@@ -28,9 +39,57 @@ public class SampleController {
 	   
 	   JSONArray data = root.getJSONArray("data");
 	   
-	   
-		
-		@CrossOrigin
+	   @CrossOrigin
+	   @GetMapping("/exercise2/{substr}")
+	   private ResponseEntity< ArrayList > exercise2(@PathVariable("substr") String substr)   
+	   {  
+		try{
+            String urlString = "https://jsonmock.hackerrank.com/api/moviesdata/search/?Title="+ substr;
+            URL url = new URL(urlString);
+            try{
+                URLConnection conn = url.openConnection();
+                BufferedReader reader;
+                String line;
+                StringBuilder responseContent = new StringBuilder();
+                reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line = reader.readLine()) != null) {
+                    responseContent.append(line);
+                }
+                reader.close();
+                JSONObject jsonObject = new JSONObject(responseContent.toString());
+				Integer total_pages = jsonObject.getInt("total_pages");
+				ArrayList titles = new ArrayList<>();
+
+				for(int i=1; i <= total_pages; i++){
+					String pageUrlString = "https://jsonmock.hackerrank.com/api/moviesdata/search/?Title="+ substr +"&page="+ i;
+					URL pageUrl = new URL(pageUrlString);
+					URLConnection connData = pageUrl.openConnection();
+					StringBuilder responseDataContent = new StringBuilder();
+					reader = new BufferedReader(new InputStreamReader(connData.getInputStream()));
+					while ((line = reader.readLine()) != null) {
+						responseDataContent.append(line);
+					}
+					reader.close();
+					JSONObject jsonDataObject = new JSONObject(responseDataContent.toString());
+					JSONArray all_data = jsonDataObject.getJSONArray("data");
+					for(int j=0; j<all_data.length(); j++){
+						JSONObject value = new JSONObject(all_data.get(j).toString());
+						titles.add(value.getString("Title"));
+					}
+				}
+                Collections.sort(titles);   
+                return new ResponseEntity<ArrayList>(titles, HttpStatus.OK);
+            }catch(Exception E){
+				System.out.println("Error encountered : "+E.getMessage());
+				return new ResponseEntity<ArrayList>(HttpStatus.NOT_FOUND);
+            }
+        }catch(Exception E){
+			System.out.println("Error encountered : "+E.getMessage());
+			return new ResponseEntity<ArrayList>(HttpStatus.NOT_FOUND);
+        }
+	   } 
+
+	   @CrossOrigin
 		@GetMapping("/filter/price/{initial_price}/{final_price}")  
 		private ResponseEntity< ArrayList<FilteredProducts> > filtered_books(@PathVariable("initial_price") int init_price , @PathVariable("final_price") int final_price)   
 		{  
@@ -60,8 +119,8 @@ public class SampleController {
    
 			}catch(Exception E)
 				{
-	   	System.out.println("Error encountered : "+E.getMessage());
-	    return new ResponseEntity<ArrayList<FilteredProducts>>(HttpStatus.NOT_FOUND);
+					System.out.println("Error encountered : "+E.getMessage());
+					return new ResponseEntity<ArrayList<FilteredProducts>>(HttpStatus.NOT_FOUND);
 				}
 			
 		}  
@@ -71,7 +130,7 @@ public class SampleController {
 		@GetMapping("/sort/price")  
 		private ResponseEntity<SortedProducts[]> sorted_books()   
 		{  
-			
+			System.out.println(data);
 			try {
 				
 		        SortedProducts[] ans=new SortedProducts[data.length()];
